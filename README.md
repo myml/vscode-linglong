b120fd13b632453be5e9b82c9650fcac04eee1b3
myml <wurongjie@deepin.org>
2024-06-06 11:46:09 +0800

    feat: add snippets

--- README.md
+++ README.md
@@ -10,7 +10,15 @@
 
 ## 依赖补全
 
-在制作玲珑包时，如果 base 和 runtime 中缺少构建应用所需依赖，需要自己添加 deb 包，deb 包的依赖繁多，软件更新后，下载地址又会失效，插件提供了一种临时方案。
+在制作玲珑包时，如果 base 和 runtime 中缺少构建应用所需依赖，需要自己添加 deb 包，deb 包的依赖繁多，软件更新后下载地址又会失效，插件提供了一种临时方案。
+
+### 第一步
+
+需要安装 aptly 这个命令行工具，下载地址 https://www.aptly.info/download/。
+
+### 第二步
+
+将 sources 字段放在 linglong.yaml 的最后，在 sources 最后添加以上注释(将 libical-dev, wget 替换称你自己想安装的软件包)
 
 ```yaml
 build: |
@@ -23,17 +31,31 @@ sources:
   # linglong:gen_deb_source install libical-dev, wget
 ```
 
-将 sources 字段放在 linglong.yaml 的最后，然后在 sources 最后添加以上注释(将 libical-dev, wget 替换称你自己想安装的软件包)
-
-第一个注释是用于设置软件源，只能写一次，第二个注释是用于安装依赖，可以写多次，例如`libical-dev, wget`可以分开写：
+第一个注释是用于设置软件源，只能写一次，第二个注释是用于安装依赖，可以写多次（多个 install 中间可添加其他内容，便于注释每个依赖的作用），例如`libical-dev, wget`可以分开写：
 
 ```
+# 构建需要
 # linglong:gen_deb_source install libical-dev
+# 用于更新节假日
 # linglong:gen_deb_source install wget
 ```
 
-添加这两行注释后，在 vscode 中按`Ctrl+Shift+P` 搜索 `linglong: Gen deb sources`, 等待一段时间后，插件会自动将填写的依赖和其依赖树全部添加到当前 linglong.yaml 文件末尾，之后依赖有更新，可重复执行
-`linglong: Gen deb sources`命令来更新软件包
+添加这两行注释后，在 vscode 中按 `Ctrl+Shift+P` 搜索 `linglong: Gen deb sources`命令, 等待一段时间后，插件会自动将填写的依赖和其依赖树全部添加到当前 linglong.yaml 文件末尾。
+
+之后依赖有更新可重复执行 `linglong: Gen deb sources` 命令来更新软件包，安装本插件后在 linglong.yaml 输入 ll(两个小写的 L,非数字 1), 可自动生成注释内容
+
+### 第三步
+
+`linglong: Gen deb sources` 命令同时会下载一个 `install_dep`脚本到工作目录，在 linglong.yaml 的 `build`字段添加 `bash ./install_dep linglong/sources "$PREFIX"`用来在构建玲珑包时安装 deb 包
+
+## 快捷命令
+
+除了 `linglong: Gen deb sources`，还添加了以下几个快捷命令，可按 `Ctrl+Shift+P` 搜索命令，并给命令设置你喜欢用的快捷键。
+
+linglong: Build
+linglong: Run
+linglong: Export
+linglong: Offline Build
 
 ## FAQ
 
--- src/extension.ts
+++ src/extension.ts
@@ -3,94 +3,68 @@ import * as fs from "fs/promises";
 import * as os from "os";
 import * as path from "path";
 
-const keywords = {
-  id: "",
-  name: "",
-  version: "",
-  package: "",
-  base: "",
-  runtime: "",
-  sources: "",
-  command: "",
-  build: "",
-  digest: "",
-  url: "",
-  kind: "",
-  description: "",
-};
-
 export function activate(context: vscode.ExtensionContext) {
-  // 代码提示
-  const triggers = [" "];
-  const documentSelector: vscode.DocumentSelector = {
-    pattern: "**/linglong.yaml",
-  };
-  const completionProvider = vscode.languages.registerCompletionItemProvider(
-    documentSelector,
-    {
-      async provideCompletionItems(
-        document: vscode.TextDocument,
-        position: vscode.Position,
-        token: vscode.CancellationToken,
-        context: vscode.CompletionContext
-      ) {
-        const range = new vscode.Range(
-          new vscode.Position(position.line, 0),
-          position
-        );
-        const text = document.getText(range);
-        const completionItems = Object.keys(keywords)
-          .filter((v) => v.startsWith(text))
-          .map((item, index) => {
-            return {
-              label: item + ": 1",
-              preselect: index === 0,
-              documentation: "玲珑插件",
-            };
-          });
-        return [];
-      },
-    },
-    ...triggers
-  );
-  context.subscriptions.push(completionProvider);
-
   // 依赖sources生成
-  let disposable = vscode.commands.registerCommand(
-    "dev.linglong.extension.gen_deb_source",
-    async () => {
-      // 获取当前活动的文本编辑器
-      const editor = vscode.window.activeTextEditor;
-      if (editor) {
-        const document = editor.document;
-        const selection = editor.selection;
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "dev.linglong.extension.gen_deb_source",
+      gen_deb_source
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "dev.linglong.extension.build",
+      builderBuild
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("dev.linglong.extension.run", builderRun)
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "dev.linglong.extension.export",
+      builderExport
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "dev.linglong.extension.build.offline",
+      builderOfflineBuild
+    )
+  );
+}
 
-        const text = document.getText();
+async function gen_deb_source() {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
+  const document = editor.document;
+  const selection = editor.selection;
+  // 构建临时文件的路径
+  const tempDir = os.tmpdir();
+  const text = document.getText();
 
-        let endline = document.lineCount;
-        let depends = "";
-        let scriptFile = "";
-        for (let [index, line] of text.split("\n").entries()) {
-          line = line.trimStart();
-          // 获取依赖
-          if (line.startsWith("# linglong:gen_deb_source install")) {
-            endline = index;
-            if (depends !== "") {
-              depends += ",";
-            }
-            depends += line.slice("# linglong:gen_deb_source install".length);
-          }
-          // 获取源
-          else if (line.startsWith("# linglong:gen_deb_source sources")) {
-            const sources = line
-              .slice("# linglong:gen_deb_source sources".length)
-              .trim();
-            const [arch, url, distribution, ...components] = sources.split(" ");
-            // 构建临时文件的路径
-            const tempDir = os.tmpdir();
-            const tempFilePath = path.join(tempDir, "get_deb_source.sh");
-            const content = `#!/bin/bash
+  let endline = document.lineCount;
+  let depends = [] as string[];
+  let scriptFile = "";
+  for (let [index, line] of text.split("\n").entries()) {
+    line = line.trimStart();
+    // 获取依赖
+    if (line.startsWith("# linglong:gen_deb_source install")) {
+      endline = index;
+      depends.push(line.slice("# linglong:gen_deb_source install".length));
+    }
+    // 获取源
+    else if (line.startsWith("# linglong:gen_deb_source sources")) {
+      const sources = line
+        .slice("# linglong:gen_deb_source sources".length)
+        .trim();
+      const [arch, url, distribution, ...components] = sources.split(" ");
+      const tempFilePath = path.join(tempDir, "get_deb_source.sh");
+      const content = `#!/bin/bash
             set -e
+            set -x
             # 去掉 [*] 和 <*> 便于从 deb 复制 Build-Depends
             pkgs=$(cat /dev/stdin | sed "s#\[[^]]\+]##g" | sed "s# <\w\+># #g" | tr ',' '|')
 
@@ -113,31 +87,60 @@ export function activate(context: vscode.ExtensionContext) {
             done
 
             rm download.log`;
-            await fs.writeFile(tempFilePath, content);
-            scriptFile = tempFilePath;
-          }
-        }
-        if (!scriptFile) {
-          return;
-        }
-        // 删除选中行到文件尾部的内容
-        const start = document.lineAt(endline + 1).range.start;
-        const end = document.lineAt(document.lineCount - 1).range.end;
-        const range = new vscode.Range(start, end);
-        await editor.edit((editBuilder) => {
-          editBuilder.delete(range);
-        });
-        document.save();
-        // 重新生成内容
-        const selectedText = document.getText(selection);
-        const terminal = vscode.window.createTerminal(`Ext Terminal`);
-        terminal.sendText(
-          `echo ${depends} | bash ${scriptFile} >> ${document.fileName}`
-        );
-        terminal.show();
-      }
+      await fs.writeFile(tempFilePath, content);
+      scriptFile = tempFilePath;
     }
-  );
+  }
+  if (!scriptFile) {
+    return;
+  }
+  if (!depends) {
+    return;
+  }
+  let dependFile = "";
+  dependFile = path.join(tempDir, "depends.list");
+  await fs.writeFile(dependFile, depends.join(","));
 
-  context.subscriptions.push(disposable);
+  // 删除选中行到文件尾部的内容
+  const start = document.lineAt(endline + 1).range.start;
+  const end = document.lineAt(document.lineCount - 1).range.end;
+  const range = new vscode.Range(start, end);
+  await editor.edit((editBuilder) => {
+    editBuilder.delete(range);
+  });
+  document.save();
+
+  const installdepUrl =
+    "https://gitee.com/deepin-community/linglong-pica/raw/master/misc/libexec/linglong/builder/helper/install_dep";
+  const terminal = vscode.window.createTerminal(`Ext Terminal`);
+  terminal.sendText(`wget -N ${installdepUrl}\n`);
+  terminal.sendText(
+    `cat ${dependFile} | bash ${scriptFile} >> ${document.fileName}\n`
+  );
+  terminal.sendText("bash -c 'rm linglong/sources/*.deb'");
+  terminal.show();
+}
+
+async function builderOfflineBuild() {
+  const terminal = vscode.window.createTerminal(`Ext Terminal`);
+  terminal.sendText(`ll-builder build --offline`);
+  terminal.show();
+}
+
+async function builderBuild() {
+  const terminal = vscode.window.createTerminal(`Ext Terminal`);
+  terminal.sendText(`ll-builder build`);
+  terminal.show();
+}
+
+async function builderRun() {
+  const terminal = vscode.window.createTerminal(`Ext Terminal`);
+  terminal.sendText(`ll-builder run`);
+  terminal.show();
+}
+
+async function builderExport() {
+  const terminal = vscode.window.createTerminal(`Ext Terminal`);
+  terminal.sendText(`ll-builder export`);
+  terminal.show();
 }
