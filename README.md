3a1d82dc80e1a52a89420e01d6b2c5a1bf58cfa0
myml <wurongjie@deepin.org>
2024-06-07 18:36:50 +0800

    feat: add gen dsc command

--- README.md
+++ README.md
@@ -48,14 +48,38 @@ sources:
 
 `linglong: Gen deb sources` 命令同时会下载一个 `install_dep`脚本到工作目录，在 linglong.yaml 的 `build`字段添加 `bash ./install_dep linglong/sources "$PREFIX"`用来在构建玲珑包时安装 deb 包
 
+## dsc 更新
+
+在制作玲珑包时，可能需要从 deb 仓库下载源码用于构建，玲珑提供了 dsc source 功能，例如在 linglong.yaml 中添加以下 source，玲珑在构建时会使用 dget 下载源码，但是 dsc 同 deb 一样容易失效，插件提供了自动更新 dsc sources 的功能
+
+```
+  - kind: dsc
+    name: qtsvg-opensource-src
+    url: https://pools.uniontech.com/deepin-beige/pool/main/q/qtsvg-opensource-src/qtsvg-opensource-src_5.15.8-1+dde.dsc
+    digest: b10b9e502c65145f7b54b691ce8ffc62400abd05898f37a803bc9af76ea55508
+```
+
+请在 sources 添加一下注释，注意 dsc_source 的注释要添加到 deb_source 的前面，避免被覆盖
+
+```
+  # linglong:gen_dsc_source sources https://pools.uniontech.com/deepin-beige beige main
+  # linglong:gen_dsc_source install qtsvg-opensource-src
+```
+
+添加这两行注释后，在 vscode 中按 `Ctrl+Shift+P` 搜索 `linglong: Gen dsc sources`命令, 等待一段时间后，插件会自动在仓库中寻找包名对应的 dsc 文件，将 dsc 文件和哈希值添加到 linglong.yaml。
+
 ## 快捷命令
 
 除了 `linglong: Gen deb sources`，还添加了以下几个快捷命令，可按 `Ctrl+Shift+P` 搜索命令，并给命令设置你喜欢用的快捷键。
 
 linglong: Build
+`ll-builder build`
 linglong: Run
+`ll-builder run`
 linglong: Export
+`ll-builder export`
 linglong: Offline Build
+`ll-builder build --offline`
 
 ## FAQ
 
--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,14 @@ import * as os from "os";
 import * as path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
-  // 依赖sources生成
+  // dsc source生成
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "dev.linglong.extension.gen_dsc_source",
+      () => gen_dsc_source(context)
+    )
+  );
+  // deb source生成
   context.subscriptions.push(
     vscode.commands.registerCommand(
       "dev.linglong.extension.gen_deb_source",
@@ -34,13 +41,36 @@ export function activate(context: vscode.ExtensionContext) {
   );
 }
 
-async function gen_deb_source() {
+async function builderOfflineBuild() {
+  const terminal = vscode.window.createTerminal(`Ext Terminal`);
+  terminal.sendText(`ll-builder build --offline`);
+  terminal.show();
+}
+
+async function builderBuild() {
+  const terminal = vscode.window.createTerminal(`Ext Terminal`);
+  terminal.sendText(`ll-builder build`);
+  terminal.show();
+}
+
+async function builderRun() {
+  const terminal = vscode.window.createTerminal(`Ext Terminal`);
+  terminal.sendText(`ll-builder run`);
+  terminal.show();
+}
+
+async function builderExport() {
+  const terminal = vscode.window.createTerminal(`Ext Terminal`);
+  terminal.sendText(`ll-builder export`);
+  terminal.show();
+}
+
+export async function gen_deb_source() {
   const editor = vscode.window.activeTextEditor;
   if (!editor) {
     return;
   }
   const document = editor.document;
-  const selection = editor.selection;
   // 构建临时文件的路径
   const tempDir = os.tmpdir();
   const text = document.getText();
@@ -50,45 +80,45 @@ async function gen_deb_source() {
   let scriptFile = "";
   for (let [index, line] of text.split("\n").entries()) {
     line = line.trimStart();
-    // 获取依赖
-    if (line.startsWith("# linglong:gen_deb_source install")) {
-      endline = index;
-      depends.push(line.slice("# linglong:gen_deb_source install".length));
-    }
-    // 获取源
-    else if (line.startsWith("# linglong:gen_deb_source sources")) {
-      const sources = line
-        .slice("# linglong:gen_deb_source sources".length)
-        .trim();
-      const [arch, url, distribution, ...components] = sources.split(" ");
-      const tempFilePath = path.join(tempDir, "get_deb_source.sh");
-      const content = `#!/bin/bash
-            set -e
-            set -x
-            # 去掉 [*] 和 <*> 便于从 deb 复制 Build-Depends
-            pkgs=$(cat /dev/stdin | sed "s#\[[^]]\+]##g" | sed "s# <\w\+># #g" | tr ',' '|')
-
-            url=${url}
-            distribution=${distribution}
-            components="${components.join(" ")}"
-            arch=${arch}
-
-            rm -rf ~/.aptly
-            aptly mirror create -ignore-signatures -architectures=$arch -filter="$pkgs" -filter-with-deps linglong-download-depend $url $distribution $components > /dev/null
-            aptly mirror update -ignore-signatures linglong-download-depend > download.log
-
-            grep 'Success downloading' download.log|grep 'deb$'|awk '{print $3}'|sort|while IFS= read -r url; do
-                filename=$(basename "$url")
-                filepath=$(find ~/.aptly/pool|grep "\_$filename")
-                digest=$(sha256sum "$filepath"|awk '{print $1}')
-                echo "  - kind: file"
-                echo "    url: $url"
-                echo "    digest: $digest"
-            done
-
-            rm download.log`;
-      await fs.writeFile(tempFilePath, content);
-      scriptFile = tempFilePath;
+    switch (true) {
+      case line.startsWith("# linglong:gen_deb_source sources"):
+        const sources = line
+          .slice("# linglong:gen_deb_source sources".length)
+          .trim();
+        const [arch, url, distribution, ...components] = sources.split(" ");
+        const tempFilePath = path.join(tempDir, "get_deb_source.sh");
+        const content = `#!/bin/bash
+                set -e
+                set -x
+                # 去掉 [*] 和 <*> 便于从 deb 复制 Build-Depends
+                pkgs=$(cat /dev/stdin | sed "s#\\[[^]]\\+]##g" | sed "s# <\\w\\+># #g" | tr ',' '|')
+    
+                url=${url}
+                distribution=${distribution}
+                components="${components.join(" ")}"
+                arch=${arch}
+    
+                rm -rf ~/.aptly
+                aptly mirror create -ignore-signatures -architectures=$arch -filter="$pkgs" -filter-with-deps linglong-download-depend $url $distribution $components > /dev/null
+                aptly mirror update -ignore-signatures linglong-download-depend > download.log
+    
+                grep 'Success downloading' download.log|grep 'deb$'|awk '{print $3}'|sort|while IFS= read -r url; do
+                    filename=$(basename "$url")
+                    filepath=$(find ~/.aptly/pool|grep "\\_$filename")
+                    digest=$(sha256sum "$filepath"|awk '{print $1}')
+                    echo "  - kind: file"
+                    echo "    url: $url"
+                    echo "    digest: $digest"
+                done
+    
+                rm download.log`;
+        await fs.writeFile(tempFilePath, content);
+        scriptFile = tempFilePath;
+        break;
+      case line.startsWith("# linglong:gen_deb_source install"):
+        endline = index;
+        depends.push(line.slice("# linglong:gen_deb_source install".length));
+        break;
     }
   }
   if (!scriptFile) {
@@ -121,26 +151,103 @@ async function gen_deb_source() {
   terminal.show();
 }
 
-async function builderOfflineBuild() {
-  const terminal = vscode.window.createTerminal(`Ext Terminal`);
-  terminal.sendText(`ll-builder build --offline`);
-  terminal.show();
-}
+export async function gen_dsc_source(context: vscode.ExtensionContext) {
+  const tempDir = os.tmpdir();
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
+  console.log(context.storageUri);
+  const document = editor.document;
+  const text = document.getText();
+  if (!vscode.workspace.workspaceFolders) {
+    return;
+  }
+  const depends: string[] = [];
+  let installBegin = 0;
+  let installEnd = 0;
+  let lastInstallCommand = 0;
+  let scriptFile = "";
+  const lines = text.split("\n");
+  for (let index = 0; index < lines.length; index++) {
+    const line = lines[index].trimStart();
+    switch (true) {
+      case line.startsWith("# linglong:gen_dsc_source sources"):
+        const sources = line
+          .slice("# linglong:gen_deb_source sources".length)
+          .trim();
+        const [url, distribution, ...components] = sources.split(" ");
+        const content = `#!/bin/bash
+          set -e
+          
+          url=${url}
+          distribution=${distribution}
+          components="${components}"
+          
+          tmpdir=$(mktemp -d)
+          cd "$tmpdir"
+          # 下载Sources文件
+          for component in $components;do
+              curl -s "$url/dists/$distribution/$component/source/Sources.gz" | gunzip >> Sources
+          done;
+          
+          while IFS= read -r pkg; do
+              # 解析Sources文件，获取pkg的存放目录
+              path=$(cat Sources | grep -E "^Package:|^Version:|^Directory:" | 
+                  awk 'BEGIN { FS = ": " } { if ($1 == "Package") { pkg = $2 } else if ($1 == "Version") { ver = $2 } else if ($1 == "Directory" && pkg=="'$pkg'") { printf "%s/%s_%s.dsc\\n",$2, pkg, ver } }')
+              echo "  - kind: dsc"
+              echo "    name: $pkg"
+              echo "    url: $url/$path"
+              echo "    digest: $(curl -s "$url/$path" | sha256sum | awk '{print $1}')"
+          done;
+          
+          rm -r "$tmpdir"`;
+        const tempFilePath = path.join(tempDir, "get_dsc_source.sh");
+        await fs.writeFile(tempFilePath, content);
+        scriptFile = tempFilePath;
+        break;
+      case line.startsWith("# linglong:gen_dsc_source install"):
+        depends.push(line.slice("# linglong:gen_deb_source install ".length));
+        lastInstallCommand = index;
+        break;
+      case line.startsWith("# linglong:gen_dsc_source begin"):
+        installBegin = index;
+        break;
+      case line.startsWith("# linglong:gen_dsc_source end"):
+        installEnd = index;
+        break;
+    }
+  }
+  console.log(lastInstallCommand, installBegin, installEnd);
+  if (installBegin > 0 && installEnd > 0) {
+    // 删除选中行到文件尾部的内容
+    const start = document.lineAt(installBegin + 1).range.start;
+    const end = document.lineAt(installEnd - 1).range.end;
+    const range = new vscode.Range(start, end);
+    await editor.edit((editBuilder) => {
+      editBuilder.replace(range, "\n");
+    });
+  } else {
+    installBegin = lastInstallCommand + 1;
+    const start = document.lineAt(installBegin).range.start;
+    await editor.edit((editBuilder) => {
+      editBuilder.insert(
+        start,
+        "# linglong:gen_dsc_source begin\n  # linglong:gen_dsc_source end\n\n"
+      );
+    });
+  }
+  await document.save();
+  let dependFile = path.join(tempDir, "dsc.list");
+  await fs.writeFile(dependFile, depends.join("\n"));
 
-async function builderBuild() {
   const terminal = vscode.window.createTerminal(`Ext Terminal`);
-  terminal.sendText(`ll-builder build`);
-  terminal.show();
-}
-
-async function builderRun() {
-  const terminal = vscode.window.createTerminal(`Ext Terminal`);
-  terminal.sendText(`ll-builder run`);
-  terminal.show();
-}
-
-async function builderExport() {
-  const terminal = vscode.window.createTerminal(`Ext Terminal`);
-  terminal.sendText(`ll-builder export`);
+  terminal.sendText(
+    `cat ${dependFile} | bash ${scriptFile} | sed -i '${
+      installBegin + 1
+    }r/dev/stdin' ${document.fileName}`
+  );
+  // 避免sed不触发vscode重新加载
+  terminal.sendText(`echo "">> linglong.yaml`);
   terminal.show();
 }
